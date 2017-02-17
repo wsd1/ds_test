6275ee41189b3cecb23c1c11bcb7c20c24ea3a00
dingyi <dy@mac>
2017-02-17 14:57:15 +0800

    New discovery.

--- record_permission_test.js
+++ record_permission_test.js
@@ -2,23 +2,23 @@
 
 function handle(success, data){
 
-  setTimeout(function(){
-    queue.discard();
-    queue.unsubscribe()
-    client.close();
-    console.log("close!");
-  }, 15000)
-
-  client.on('error', function( error, event, topic ){
-    console.log("Error evt:" + error, event, topic);
-  })
-
-  client.on( 'connectionStateChanged', function( connectionState ){
-    console.log("State:" + connectionState);
-  });
-
-
   if (success) {
+    console.log("Login success!!!!");
+
+    setTimeout(function(){
+      queue.discard();
+      queue.unsubscribe()
+      client.close();
+      console.log("close!");
+    }, 15000)
+
+    client.on('error', function( error, event, topic ){
+      console.log("[Error evt]:" + error, event, topic);
+    })
+
+    client.on( 'connectionStateChanged', function( connectionState ){
+      console.log("[State evt]:" + connectionState);
+    });
 
     // start application
     // client.getConnectionState() will now return 'OPEN'
@@ -28,16 +28,18 @@ function handle(success, data){
 
     var queue = client.record.getRecord("app.catcher/queue/1234");
 
+
     queue.whenReady(function(){
+
+
 
       var old = queue.get();
       console.log("Record got, Value before set:" + old);
 
-      console.log("Try subscribing....");
-
-      queue.subscribe(function(data){
-      	console.log("Queue change: " + data);
-      })
+//      console.log("Try subscribing....");
+//      queue.subscribe(function(data){
+//        console.log("Queue change: " + data);
+//      })
 
 
       queue.set(['a','app.catch', Math.random()], function(err){
@@ -51,31 +53,36 @@ function handle(success, data){
 
 
       setTimeout(function(){
-	      var old = queue.get();
-	      console.log("Record got, Value before 2nd set:" + old);
 
-	      queue.set(['a','app.catcher', Math.random()], function(err){
-	        if (err) {
-	          console.log('Record 2nd set Error:', err)
-	        } else {
-	          console.log('Record 2nd set OK')
-	        }
-	        console.log("After set:" + queue.get());
-	      })
+        var old = queue.get();
+        console.log("Record got, Value before 2nd set:" + old);
+
+        queue.set(['a','app.catcher', Math.random()], function(err){
+          if (err) {
+            console.log('Record 2nd set Error:', err)
+          } else {
+            console.log('Record 2nd set OK')
+          }
+          console.log("After set:" + queue.get());
+        })
+
+
       }, 5000);
 
       setTimeout(function(){
-	      var old = queue.get();
-	      console.log("Record got, Value before 3rd set:" + old);
 
-	      queue.set(['a','app.catcher', Math.random()], function(err){
-	        if (err) {
-	          console.log('Record 3rd set Error:', err)
-	        } else {
-	          console.log('Record 3rd set OK')
-	        }
-	        console.log("After set:" + queue.get());
-	      })
+        var old = queue.get();
+        console.log("Record got, Value before 3rd set:" + old);
+
+        queue.set(['a','app.catcher', Math.random()], function(err){
+          if (err) {
+            console.log('Record 3rd set Error:', err)
+          } else {
+            console.log('Record 3rd set OK')
+          }
+          console.log("After set:" + queue.get());
+        })
+
       }, 10000);
 
 
